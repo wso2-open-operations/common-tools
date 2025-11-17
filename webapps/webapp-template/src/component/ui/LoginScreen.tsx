e80ae2ac3d8b53c826c67a59bc1b27255e5031e4
dinethde <dinethran33@gmail.com>
2025-11-17 20:08:45 +0530

    fix: add alt attribute to product logos image for accessibility

--- webapps/webapp-template/src/component/ui/LoginScreen.tsx
+++ webapps/webapp-template/src/component/ui/LoginScreen.tsx
@@ -116,7 +116,7 @@ const LoginScreen = () => {
                       Powered By
                     </Typography>
                     <Stack direction="row" spacing={2}>
-                      <img height={22} src={ProductLogos} />
+                      <img height={22} alt="Product logos" src={ProductLogos} />
                     </Stack>
                   </Stack>
                 </Grid>
