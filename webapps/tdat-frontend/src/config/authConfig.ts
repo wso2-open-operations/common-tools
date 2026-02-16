1a4150284c44747ea469ed5cb80f0ee24ff50766
Sakiththiyan <sakiththiyan@wso2.com>
2026-02-16 10:15:23 +0530

    fix: authConfig.ts

--- webapps/tdat-frontend/src/config/authConfig.ts
+++ webapps/tdat-frontend/src/config/authConfig.ts
@@ -1,7 +1,7 @@
 {/* Asgardeo related Configs*/}
 const authConfig = {
-    signInRedirectURL: "window.location.origin", //"http://localhost:5173", 
-    signOutRedirectURL: "window.location.origin", //"http://localhost:5173",
+    signInRedirectURL: window.location.origin, //"http://localhost:5173", 
+    signOutRedirectURL: window.location.origin, //"http://localhost:5173",
     clientID: "k18v4HF2HlbfJMpi6tSQlwsYA_0a", 
     baseUrl: "https://api.asgardeo.io/t/troubleshootingtools",
     scope: [ "openid", "email", "profile" ]
