f4d85e1b4887af5b263bb19d69ddf7e39808f447
Sakiththiyan <sakiththiyan@wso2.com>
2026-02-16 10:29:16 +0530

    fixes in baseUrl

--- webapps/tdat-frontend/src/config/authConfig.ts
+++ webapps/tdat-frontend/src/config/authConfig.ts
@@ -2,8 +2,8 @@
 const authConfig = {
     signInRedirectURL: window.location.origin, //"http://localhost:5173", 
     signOutRedirectURL: window.location.origin, //"http://localhost:5173",
-    clientID: "k18v4HF2HlbfJMpi6tSQlwsYA_0a", 
-    baseUrl: "https://api.asgardeo.io/t/troubleshootingtools",
+    clientID: "ljAEYPjEWlP2r1gxTUfMIXrFhUIa", 
+    baseUrl: "https://api.asgardeo.io/t/cstroubleshootingtools",
     scope: [ "openid", "email", "profile" ]
 };
 
